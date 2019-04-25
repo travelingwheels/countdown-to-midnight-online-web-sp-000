3ba76afde292f998a04244fd8bd4f5ae7160f758
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-25 13:51:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+
+def countdown(number)
+  number = 10
+  while number > 0
+    puts "#{number} second(s)!"
+     number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+  
 end
